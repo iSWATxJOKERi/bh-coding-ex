a97c13c1618153952f7740f523f2694531f80f3e
iSWATxJOKERi <iswatxjokeri@gmail.com>
2024-08-27 13:24:22 -0700

    add two lines

--- render-build.sh
+++ render-build.sh
@@ -5,8 +5,6 @@ set -o errexit
 cd backend
 
 bundle install
-bundle exec rails assets:precompile
-bundle exec rails assets:clean
 
 # If you're using a Free instance type, you need to
 # perform database migrations in the build command.
